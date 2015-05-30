71255d1770fe75f06c4c1c94f7c91a2d0a8940cd
Hugo Monteiro <hugo.monteiro@gmail.com>
2015-05-30 14:03:21 +0100

    right order of "uses" imports

--- DependencyInjection/EmailApiExtension.php
+++ DependencyInjection/EmailApiExtension.php
@@ -2,11 +2,11 @@
 
 namespace HP\Bundle\EmailApiBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -22,7 +22,6 @@ class EmailApiExtension extends Extension
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
-
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
